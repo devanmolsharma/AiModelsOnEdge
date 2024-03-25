1e4c58ca4f2a527253dfe22348b9e3fdeb888663
Anmol Sharma <keepupsharma@gmail.com>
2024-03-24 20:15:59 -0500

    added a few functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 #include "tensor.c"
-int shape1[5] = {2, 3, 4, 5, 6};
-float data1[] = {1.0, 2.0, 3.0, 120.0};
+int shape1[5] = {1,1};
+double data1[] = {1.0, 2.0, 3.0, 120.0};
 
 // Create a new instance of Tensor
 struct Tensor t = createTensor(shape1,sizeof(data1)/sizeof(float),data1);
 
 
-int shape2[5] = {2, 3, 4, 5, 6};
-float data2[] = {1.0, 2.0, 3.0, 120.0};
+int shape2[5] = {1,1};
+double data2[] = {1.0, 2.0, 3.0, 120.0};
 
 // Create a new instance of Tensor
 struct Tensor t2 = createTensor(shape2,sizeof(data2)/sizeof(float),data2);
@@ -20,7 +20,7 @@ void setup()
 
 void loop()
 {
-  add(t,t2,&t);
+  multiply_number(t2,2,&t);
   Serial.printf("%2f %2f %2f %2f\n",t.data[0],t.data[1],t.data[2],t.data[3]);
-  delay(20000);
+  delay(6e4);
 }
--- src/tensor.c
+++ src/tensor.c
@@ -1,21 +1,34 @@
 #include <stdlib.h>
-
+#include <math.h>
 struct Tensor
 {
     int shape[5];
     int size;
-    float *data;
+    double *data;
 };
 
-struct Tensor createTensor(int shape[5], int size, float *data)
+struct Tensor createTensor(int shape[5], int size, double *data)
 {
     struct Tensor t;
+
+    int total_size = 0;
+
     for (int i = 0; i < 5; i++)
     {
-        t.shape[i] = shape[i];
+        int s = shape[i];
+        t.shape[i] = s;
+        if (s != 0)
+        {
+            total_size *= s;
+        }
     }
 
-    t.data = (float *)malloc(size * (sizeof(float)));
+    if (total_size != size)
+    {
+        // Handle this somehow
+    }
+
+    t.data = (double *)malloc(size * (sizeof(double)));
     for (int i = 0; i < size; i++)
     {
         t.data[i] = data[i];
@@ -26,7 +39,7 @@ struct Tensor createTensor(int shape[5], int size, float *data)
     return t;
 }
 
-void setTensor(struct Tensor * t, int shape[5], int size, float *data)
+void setTensor(struct Tensor *t, int shape[5], int size, double *data)
 {
     for (int i = 0; i < 5; i++)
     {
@@ -41,10 +54,10 @@ void setTensor(struct Tensor * t, int shape[5], int size, float *data)
     t->size = size;
 }
 
-void add(struct Tensor t1, struct Tensor t2, struct Tensor * out)
+void add(struct Tensor t1, struct Tensor t2, struct Tensor *out)
 {
 
-    float arr3[t1.size];
+    double arr3[t1.size];
 
     for (int i = 0; i < t1.size; i++)
     {
@@ -54,10 +67,10 @@ void add(struct Tensor t1, struct Tensor t2, struct Tensor * out)
     setTensor(out, t1.shape, t1.size, arr3);
 };
 
-void multiply(struct Tensor t1, struct Tensor t2, struct Tensor * out)
+void multiply(struct Tensor t1, struct Tensor t2, struct Tensor *out)
 {
 
-    float arr3[t1.size];
+    double arr3[t1.size];
 
     for (int i = 0; i < t1.size; i++)
     {
@@ -67,10 +80,10 @@ void multiply(struct Tensor t1, struct Tensor t2, struct Tensor * out)
     setTensor(out, t1.shape, t1.size, arr3);
 };
 
-void divide_tensors(struct Tensor t1, struct Tensor t2, struct Tensor * out)
+void divide_tensors(struct Tensor t1, struct Tensor t2, struct Tensor *out)
 {
 
-    float arr3[t1.size];
+    double arr3[t1.size];
 
     for (int i = 0; i < t1.size; i++)
     {
@@ -80,10 +93,10 @@ void divide_tensors(struct Tensor t1, struct Tensor t2, struct Tensor * out)
     setTensor(out, t1.shape, t1.size, arr3);
 };
 
-void subtract(struct Tensor t1, struct Tensor t2, struct Tensor * out)
+void subtract(struct Tensor t1, struct Tensor t2, struct Tensor *out)
 {
 
-    float arr3[t1.size];
+    double arr3[t1.size];
 
     for (int i = 0; i < t1.size; i++)
     {
@@ -93,7 +106,96 @@ void subtract(struct Tensor t1, struct Tensor t2, struct Tensor * out)
     setTensor(out, t1.shape, t1.size, arr3);
 };
 
+void softmax(struct Tensor t1, struct Tensor *out)
+{
 
-void destroy(struct Tensor t){
+    double arr3[t1.size];
+    double sum = 0;
+    const double E = 2.71828;
+
+    for (int i = 0; i < t1.size; i++)
+    {
+        double e_x = pow(E, (t1.data)[i] * -1);
+        arr3[i] = e_x;
+        sum += e_x;
+    }
+
+    for (int i = 0; i < t1.size; i++)
+    {
+        arr3[i] /= sum;
+    }
+
+    setTensor(out, t1.shape, t1.size, arr3);
+};
+
+void sigmoid(struct Tensor t1, struct Tensor *out)
+{
+
+    double arr3[t1.size];
+    const double E = 2.71828;
+
+    for (int i = 0; i < t1.size; i++)
+    {
+        double e_x_plus_one = 1 + pow(E, (t1.data)[i] * -1);
+        arr3[i] = ((double)1) / e_x_plus_one;
+    }
+
+    setTensor(out, t1.shape, t1.size, arr3);
+};
+
+void destroy(struct Tensor t)
+{
     free(t.data);
 }
+
+void add_number(struct Tensor t1, double t2, struct Tensor *out)
+{
+
+    double arr3[t1.size];
+
+    for (int i = 0; i < t1.size; i++)
+    {
+        arr3[i] = t1.data[i] + t2;
+    }
+
+    setTensor(out, t1.shape, t1.size, arr3);
+};
+
+void multiply_number(struct Tensor t1, double t2, struct Tensor *out)
+{
+
+    double arr3[t1.size];
+
+    for (int i = 0; i < t1.size; i++)
+    {
+        arr3[i] = (t1.data)[i] * t2;
+    }
+
+    setTensor(out, t1.shape, t1.size, arr3);
+};
+
+void divide_number(struct Tensor t1, double t2, struct Tensor *out)
+{
+
+    double arr3[t1.size];
+
+    for (int i = 0; i < t1.size; i++)
+    {
+        arr3[i] = (t1.data)[i] / t2;
+    }
+
+    setTensor(out, t1.shape, t1.size, arr3);
+};
+
+void subtract_number(struct Tensor t1, double t2, struct Tensor *out)
+{
+
+    double arr3[t1.size];
+
+    for (int i = 0; i < t1.size; i++)
+    {
+        arr3[i] = (t1.data)[i] - t2;
+    }
+
+    setTensor(out, t1.shape, t1.size, arr3);
+};
